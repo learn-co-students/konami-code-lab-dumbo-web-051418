f6a89fdb1bb9075ec55d263a0a7effaa5cc32760
prmoreira23 <prm.gredes@gmail.com>
2018-06-27 14:44:02 -0400

    I hate alert().

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,29 @@ const codes = [
 
 function init() {
   // your code here
+  window.addEventListener('keydown', () => alert("oi"));
+  // alert("sadasd");
 }
+
+// Keep track of index outside of the event handler.
+let index = 0;
+
+function onKeyDownHandler(e) {
+  alert("test");
+  const key = e.key;
+  console.log(key, index);
+  console.log("CODE LENGTH: ", codes.length)
+  if (key === codes[index]) {
+    // index++;
+    if (index++ === codes.length - 1) {
+      console.log("I got the message");
+      index = 0;
+      alert("You did it!");
+    }
+  } else {
+    index = 0;
+  }
+  e.preventDefault();
+}
+
+init();
